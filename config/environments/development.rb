b1da519c21b78420fdb4db9da13127cb5d0c3299
bilguun <bilquun3@gmail.com>
2023-01-16 05:28:08 +0900

    import number 2 the README file

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -55,11 +55,10 @@ Rails.application.configure do
 
   # Annotate rendered view with file names.
   # config.action_view.annotate_rendered_view_with_filenames = true
-
+  
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
-  config.file_watcher = ActiveSupport::EventedFileUpdateChecker
   # Cloud9 への接続を許可する
-
+  config.file_watcher = ActiveSupport::EventedFileUpdateChecker
   config.hosts.clear
 end
